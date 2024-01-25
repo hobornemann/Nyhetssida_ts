d0fbb6edec5e4afb42deab9f4363c987a9694642
Hans-Olov <hobornemann@gmail.com>
2024-01-25 23:22:30 +0100

    created a folder for global types

---
+++ src/types/article.d.ts
@@ -0,0 +1,16 @@
+
+// TYPE
+type Article = {
+    author: string | null;   
+    content: string | null;
+    description: string | null;
+    publishedAt: string | null;
+    source: {
+      id: string | null;
+      name: string | null;
+    };
+    title: string | null;
+    url: string | null;
+    urlToImage: string | null;
+    isFavourite?: boolean | null;
+  };

2593b0c8f180d2811eb07f2b7f4425859788c2ff
Isak Tilahun <you@example.com>
2024-01-26 16:33:39 +0100

    Sök med nyckelord samt datum i inputfältet.

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,58 @@ import { getNewsData } from "./modules/render-news";
 
 
 getNewsData(); 
-// --------------------------Header-menu--------------------------------
-const header = document.querySelector('.filter-cont'); 
-const headerMenu = document.querySelector('.top-right-menu'); 
-headerMenu.addEventListener('click', () => {
-    header.classList.toggle('show-menu')
+// ------------------------------Header--------------------------------
+const header = document.querySelector('header')!;
+const dateInput = document.querySelector<HTMLInputElement>('.date-for-filter-search')!; 
+header.addEventListener('click', (el: MouseEvent) => {
+    const optionsInHeader: HTMLLIElement = document.querySelector('.filter-cont')!; 
+    const headerMenu = document.querySelector('.top-right-menu'); 
+    const target: EventTarget | null = el.target; 
+    // console.log(dateInput.value)
+    if(target === headerMenu) return optionsInHeader.classList.toggle('show-menu'); 
+    if(target === headerInput){
+        return dateInput.classList.add('show-date');
+    } else if(dateInput.classList.contains('show-date') && target !== dateInput){
+        return dateInput.classList.remove('show-date');
+    }
 })
 
+// ---------------------------Search for articles-----------------------------
+const headerInput = document.querySelector<HTMLInputElement>('#header-input')
+const formInHeader = document.querySelector<HTMLInputElement>('.form-in-header')!
+formInHeader.addEventListener('submit', (el: MouseEvent) => {
+    el.preventDefault(); 
+    const keyWord: string | null = headerInput.value; 
+    const date: string | null = dateInput.value; 
+    headerInput.value = '' 
+
+    if(keyWord){
+       const url: string = `https://newsapi.org/v2/everything?q=${keyWord}&from=${date}&sortBy=popularity&apiKey=${import.meta.env.VITE_NEWS_API}`
+       getNewsData(url); 
+        return
+    } else if(!keyWord){
+        headerInput.value = 'Input is empty..';
+        setTimeout(() => {
+            headerInput.value = '';
+        }, 5000);
+    }
+}); 
+
 // ----------------------SHOW MORE BUTTON--------------------------------
-const mainContentContainer = document.querySelector('.main-news-content'); 
+const mainContentContainer = document.querySelector<HTMLUListElement>('.main-news-content')!; 
 mainContentContainer.addEventListener('click', (el) => {
-    const showMoreIkon = document.querySelectorAll('.show-more-cont img'); 
+    const showMoreIcon = document.querySelectorAll<HTMLImageElement>('.show-more-cont img'); 
     const showMoreContent = document.querySelectorAll('.content');
     const target = el.target;
     console.log(target)
     
-    showMoreIkon.forEach((ikon,index) => {
-        if(target === ikon){
+    showMoreIcon.forEach((icon:HTMLImageElement,index:number) => {
+        if(target === icon){
             showMoreContent[index].classList.toggle('show-more');
             
             if(showMoreContent[index].classList.contains('show-more')) 
-                return ikon.style.transform ='rotate(180deg)'; 
-            return ikon.style.transform ='rotate(0deg)'
+                return icon.style.transform ='rotate(180deg)'; 
+            return icon.style.transform ='rotate(0deg)'
         }
     })
 }) 
--- src/modules/render-news.ts
+++ src/modules/render-news.ts
@@ -4,9 +4,10 @@
 import axios from "axios";
 import { Article, Articles } from "../types/article";
 
-export async function getNewsData(){
+export async function getNewsData(url=null){
   const APIkey: string = import.meta.env.VITE_NEWS_API; 
-  const URL: string = `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${APIkey}`
+  const URL: string = (url) ? 
+  url : `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${APIkey}`
 
   try {
     const response = await axios(URL)
@@ -21,23 +22,30 @@ export async function getNewsData(){
 async function renderNewsHTML(data: Articles){
   // data parametern kommer med en property döpt till article, som innehåller en lista med 10 objekt (Nyheter). 
   // Viktiga properties --> author, content, description, title, urlToImage och publishedAt (date).  
+  const newsCont: HTMLUListElement | null = document.querySelector('.main-news-content'); 
+  if(newsCont && data.articles.length < 1){
+    return newsCont.innerHTML = "Unfortunately, there are no news articles available for the choosen date. Please check back later for updates."
+  }  
+
   const html = data.articles.map((article: Article) => {
-    let {author, urlToImage, source: name, title, description, content } = article;
+    let {author, urlToImage, source: {name}, title, description, content } = article;
     
     if(author === null || urlToImage === null || name === null || title === null || description === null || content === null )
     return // Objekt med null inuti ska ej visas på skärmen
   
   if(content)  
     content = content.substring(0,content.indexOf('[')); 
-  console.log("article in renderNewsHTML",article)
+  // console.log("article in renderNewsHTML",article)
 
     return `
     <li class="article-container">
-      <h1> Author: ${author}</h1>
+      <h1>${name}</h1>
       <div>
         <div class="article-content">
+          
           <img src="${urlToImage}" alt="${name} headline picture">
           <div class="title-container">
+          <p><b>Author: ${author}</b></p>
             <h2 class="title">${title}</h2>
             <p class="sub-title">${description}</p>
           </div>
@@ -56,6 +64,6 @@ async function renderNewsHTML(data: Articles){
     `
   }).join(''); 
 
-  const newsCont: HTMLUListElement | null = document.querySelector('.main-news-content'); 
+  
   if(newsCont) newsCont.innerHTML = html; 
 }

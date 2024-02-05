934ca7cbdf8326161074ebeaa6ade8232e8d34c6
Hans-Olov <hobornemann@gmail.com>
2024-02-05 14:33:03 +0100

    sw.ts file WIP

---
+++ src/sw.ts
@@ -0,0 +1,33 @@
+// @ts-nocheck
+
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+    '/',
+    '/style/article.css',
+    '/style/media-queries.css',
+    '/style/pages.css',
+    '/style/style.css',
+    '/types/article.d.ts',
+    '/types/index.d.ts',
+    '/modules/favourites.ts',
+    '/modules/localStorage.ts',
+    '/modules/model.ts',
+    '/modules/render-news.ts',
+    '/svg-icon/account-svgrepo-com.svg',
+    'svg-icon/american-football-svgrepo-com.svg',
+    'svg-icon/arrow-down-circle-svgrepo-com.svg',
+    'svg-icon/calender-svgrepo-com.svg',
+    'svg-icon/chevron-left-svgrepo-com.svg',
+    'svg-icon/favourite-star-black-svgrepo-com.svg',
+    'svg-icon/favourite-star-red-svgrepo-com.svg',
+    'svg-icon/favourite-star-svgrepo-com.svg',
+    'svg-icon/menu-svgrepo-com.svg',
+    'svg-icon/news-svgrepo-com.svg',
+    'svg-icon/search-svgrepo-com (1).svg',
+    'vite-env-d.ts',
+    'index.html',
+    'package-lock.json',
+    'package.json',
+    'tsconfig.json',
+    'typescript.svg'
+]
